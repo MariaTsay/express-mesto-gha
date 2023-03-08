c522aac8670910f38fde047f7bcaf01d781f09a3
Maria Tsay <mashuny2@yandex.ru>
2023-03-08 19:50:09 +0300

    ПР13: express

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,7 +26,7 @@ module.exports.deleteCard = (req, res) => {
     .orFail(() => res.status(404).send({ message: 'Карточка с указанным  _id не найдена' }))
     .then((result) => res.status(200).send(result))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для создания карточки' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
