d2d252e6d8b04cb5f7484b281a4e4b945852f473
Maria Tsay <mashuny2@yandex.ru>
2023-03-08 23:50:48 +0300

    ПР13: express

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const http2 = require('http2');
+
+const {
+  HTTP_STATUS_NOT_FOUND,
+} = http2.constants;
 
 const userRouter = require('./users');
 const cardRouter = require('./cards');
@@ -6,7 +11,7 @@ const cardRouter = require('./cards');
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+  res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Страница не найдена' });
 });
 
 module.exports = router;
