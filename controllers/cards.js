101bcbdf51e9b419e440dd84a680b232bea5f625
Maria Tsay <mashuny2@yandex.ru>
2023-03-18 20:01:28 +0300

    ПР14: авторизация express

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,16 +1,9 @@
-const http2 = require('http2');
 const { default: mongoose } = require('mongoose');
 
 const NotFound = require('../errors/NotFound');
 const BadRequest = require('../errors/BadRequest');
 const Forbidden = require('../errors/Forbidden');
 const Card = require('../models/card');
-
-const {
-  HTTP_STATUS_BAD_REQUEST,
-  HTTP_STATUS_NOT_FOUND,
-  HTTP_STATUS_INTERNAL_SERVER_ERROR,
-} = http2.constants;
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
@@ -53,14 +46,13 @@ module.exports.deleteCard = (req, res, next) => {
     .catch(next);
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   if (!mongoose.isValidObjectId(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )) {
-    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
-    return;
+    throw new BadRequest('Переданы некорректные данные');
   }
 
   Card.findByIdAndUpdate(
@@ -68,19 +60,20 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с указанным  _id не найдена' }))
+    .orFail(() => {
+      throw new NotFound('Карточка с указанным  _id не найдена');
+    })
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' }));
+    .catch(next);
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   if (!mongoose.isValidObjectId(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )) {
-    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
-    return;
+    throw new BadRequest('Переданы некорректные данные');
   }
 
   Card.findByIdAndUpdate(
@@ -88,7 +81,9 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с указанным  _id не найдена' }))
+    .orFail(() => {
+      throw new NotFound('Карточка с указанным  _id не найдена');
+    })
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' }));
+    .catch(next);
 };
