a986288fff9427f246aa88d3447b9d0710c3657f
Maria Tsay <mashuny2@yandex.ru>
2023-03-18 19:24:38 +0300

    ПР14: авторизация express

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
 
+const { Joi, celebrate, errors } = require('celebrate');
 const {
   getUsers, getUserId, getCurrentUser, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
-router.get('/:userId', getUserId);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), getUserId);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), updateAvatar);
+
+router.use(errors());
 
 module.exports = router;
